2220b78b76d70a6652281ede09e9afbcbcbcebc7
adawiyahajr <adawiyahajr@gmail.com>
2022-03-19 17:18:57 +0700

    mengerjakan latihan di pertemuan 6

---
+++ kuliah/pertemuan7/latihan.php
@@ -0,0 +1,12 @@
+<?php 
+// SUPERGLOBALS
+// variabel global milik PHP
+// merupakan array associative
+// echo $_SERVER["SERVER_NAME"];
+
+// $_GET
+$_GET["nama"] = "adawiyah ajriah";
+$_GET["nrp"] = "213040054";
+var_dump($_GET);
+
+?>
